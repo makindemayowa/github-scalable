12cd920406ff4291522474a70bbf7712c1fabf2c
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2018-06-02 00:35:46 +0100

    heroku new fix begins

--- build/index.js
+++ build/index.js
@@ -1,11 +1,23 @@
 import Koa from 'koa'
+import path from 'path'
 import webpack from 'webpack'
+import serve from 'koa-static'
 import historyApiFallback from '@overra/koa-history-api-fallback'
 import { devMiddleware, hotMiddleware } from 'koa-webpack-middleware'
 import devConfig from './webpack.dev'
 import prodConfig from './webpack.prod'
 
+const Router = require('koa-router')
+
 const app = new Koa()
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'Hello World!'
+})
+
+app.use(router.routes()).use(router.allowedMethods())
+app.use(serve(path.join(__dirname, '../dist/')))
 
 const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
 const compiler = webpack(config)
